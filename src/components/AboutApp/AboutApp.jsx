313e6e1407852bccc55d1e2bcc90757a0a18ba71
RowanKinross <rowankinross@yahoo.co.uk>
2024-02-28 16:24:37 +0000

    dayjs added to answers page

--- src/components/AboutApp/AboutApp.jsx
+++ src/components/AboutApp/AboutApp.jsx
@@ -2,10 +2,10 @@
 function AboutApp() {
   return (
     <div className='aboutApp'>
-              <h2>About the App</h2>
-              <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui reiciendis cupiditate quam consectetur velit libero minima, in quo, veniam ipsam sapiente dolorem similique omnis ratione. Eum consequuntur assumenda eaque et! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id atque obcaecati saepe recusandae quasi delectus beatae, natus exercitationem aspernatur molestias neque reiciendis sed magnam ipsam iusto iure voluptatibus consectetur sequi.
-              </p>
+      <h2>About the App</h2>
+      <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui reiciendis cupiditate quam consectetur velit libero minima, in quo, veniam ipsam sapiente dolorem similique omnis ratione. Eum consequuntur assumenda eaque et! Lorem ipsum dolor sit amet consectetur adipisicing elit. Id atque obcaecati saepe recusandae quasi delectus beatae, natus exercitationem aspernatur molestias neque reiciendis sed magnam ipsam iusto iure voluptatibus consectetur sequi.
+      </p>
     </div>
   )
 }
