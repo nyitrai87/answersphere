19c6243bc1dcd649e47d906fdc5959c3c1788f4f
Anna Chernova <annanvks@gmail.com>
2024-02-28 23:01:05 +0000

    added logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,59 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink } from "react-router-dom";
+import appLogo from "../../../public/images/logoBagel.png";
 
 function Header() {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary" fixed='top'>
+      <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
         <Container>
-          <Navbar.Brand href="/">Answer Sphere</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+              src={appLogo}
+              alt="App Logo"
+              style={{ height: "50px", marginRight: "10px" }}
+            />
+            AnswerSphere
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <NavLink to="" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+              <NavLink
+                to=""
+                end
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
                 Home
               </NavLink>
-              <NavLink to="/about" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+              <NavLink
+                to="/about"
+                end
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
                 About the App
               </NavLink>
-              <NavLink to="/contact" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+              <NavLink
+                to="/contact"
+                end
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
                 About Us
               </NavLink>
-              <NavLink to="/answers" end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+              <NavLink
+                to="/answers"
+                end
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+              >
                 Answers
               </NavLink>
             </Nav>
