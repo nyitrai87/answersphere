c83282ddcf9f114a9879094f2cda6bed6da8c047
Ioana Iosub <stu.waltham@gmail.com>
2024-03-03 13:39:32 +0000

    form hidden and message is displayed, buttons added

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,17 +2,78 @@ import { Card, Button, Form, Container } from "react-bootstrap";
 import "./index.css";
 
 import OpenAI from "openai";
+import { useState } from "react";
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_KEY,
   dangerouslyAllowBrowser: true,
 });
 
+function QuestionForm({ onSubmit }) {
+  return (
+    <Form className="py-5" onSubmit={onSubmit}>
+      <Form.Group className="mb-3 row">
+        <div className="col-12 col-lg-9">
+          <Form.Control
+            type="text"
+            placeholder="What is your name?"
+            name="name"
+          />
+        </div>
+      </Form.Group>
+
+      <Form.Group className="mb-3 row">
+        <div className="col-12 col-lg-9">
+          <Form.Select
+            className="w-100"
+            aria-label="Select what you need from the universe today"
+            name="need"
+          >
+            <option>What do you need today?</option>
+            <option value="question">Ask a question</option>
+            <option value="advice">Get advice</option>
+            <option value="inspiration">Find inspiration</option>
+            <option value="affirmation">Daily affirmation</option>
+            <option value="surprise">Surprise me</option>
+            <option value="ask">Decide: Yes or No</option>
+          </Form.Select>
+        </div>
+      </Form.Group>
+
+      <Form.Group className="mb-3 row">
+        <div className="col-12 col-lg-9">
+          <Form.Control
+            type="text"
+            placeholder="Share what's on your mind..."
+            name="text"
+          />
+        </div>
+      </Form.Group>
+
+      <Button
+        variant="primary"
+        type="submit"
+        className="roboto-bold custom-btn ask-btn"
+        style={{
+          backgroundColor: "#3BA1C8",
+          padding: "10px 22px",
+          marginTop: "20px",
+          color: "white",
+          border: "none",
+          borderRadius: "20px",
+        }}
+      >
+        Ask the Universe
+      </Button>
+    </Form>
+  );
+}
+
 function Home() {
-  //Handle form
+  const [answer, setAnswer] = useState();
   async function handleFormSubmit(e) {
     e.preventDefault();
-    //User input values
+
     const formData = new FormData(e.target);
     const formValues = Object.fromEntries(formData.entries());
 
@@ -27,8 +88,7 @@ function Home() {
       model: "gpt-3.5-turbo",
     });
 
-    console.log(chatCompletion);
-    console.log(formValues);
+    setAnswer(chatCompletion.choices[0].message.content);
   }
 
   return (
@@ -50,63 +110,15 @@ function Home() {
         </Card>
 
         <Container className="ask-container">
-          <Form className="py-5" onSubmit={handleFormSubmit}>
-            <Form.Group className="mb-3 row">
-              <div className="col-12 col-lg-9">
-                <Form.Control
-                  type="text"
-                  placeholder="What is your name?"
-                  name="name"
-                />
-              </div>
-            </Form.Group>
-
-            <Form.Group className="mb-3 row">
-              <div className="col-12 col-lg-9">
-                <Form.Select
-                  className="w-100"
-                  aria-label="Select what you need from the universe today"
-                  name="need"
-                >
-                  <option>
-                    What do you need today?
-                  </option>
-                  <option value="question">Ask a question</option>
-                  <option value="advice">Get advice</option>
-                  <option value="inspiration">Find inspiration</option>
-                  <option value="affirmation">Daily affirmation</option>
-                  <option value="surprise">Surprise me</option>
-                  <option value="ask">Decide: Yes or No</option>
-                </Form.Select>
-              </div>
-            </Form.Group>
-
-            <Form.Group className="mb-3 row">
-              <div className="col-12 col-lg-9">
-                <Form.Control
-                  type="text"
-                  placeholder="Share what's on your mind..."
-                  name="text"
-                />
-              </div>
-            </Form.Group>
-
-            <Button
-              variant="primary"
-              type="submit"
-              className="roboto-bold custom-btn ask-btn"
-              style={{
-                backgroundColor: "#3BA1C8",
-                padding: "10px 22px",
-                marginTop: "20px",
-                color: "white",
-                border: "none",
-                borderRadius: "20px",
-              }}
-            >
-              Ask the Universe
-            </Button>
-          </Form>
+          {answer ? (
+            <>
+              <div>{answer}</div>
+              <Button>Retry</Button>
+              <Button onClick={() => setAnswer(undefined)}>New question</Button>
+            </>
+          ) : (
+            <QuestionForm onSubmit={handleFormSubmit} />
+          )}
         </Container>
       </Container>
     </>
