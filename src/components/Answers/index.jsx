3d480bc2683b85e4b2d8fd93512e46c57a25f1c8
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-03-05 22:28:40 +0100

    managed to retrieve stored data from Firestore

--- src/components/Answers/index.jsx
+++ src/components/Answers/index.jsx
@@ -1,24 +1,44 @@
+import { useState, useEffect } from 'react';
+import { useAuth } from "../../contexts/authContext";
+import firebase from 'firebase/compat/app';
+import 'firebase/firestore';
 import dayjs from "dayjs";
 const Today = dayjs().format("DD/MM/YYYY");
 import Container from "react-bootstrap/Container";
 
 function AnswerPage() {
+  const { currentUser } = useAuth();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (currentUser) {
+      const fetchData = async () => {
+        const db = firebase.firestore();
+        const snapshot = await db.collection('answers')
+          .where('userId', '==', currentUser.uid)
+          .get();
+        const fetchedData = snapshot.docs.map(doc => doc.data());
+        setData(fetchedData);
+      };
+
+      fetchData();
+    }
+  }, [currentUser]);
+
   return (
     <>
       <Container>
         <div className="AnswerPage">
           <p>{Today}</p>
-          <h2>Answers</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
-            reiciendis cupiditate quam consectetur velit libero minima, in quo,
-            veniam ipsam sapiente dolorem similique omnis ratione. Eum
-            consequuntur assumenda eaque et! Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Id atque obcaecati saepe recusandae
-            quasi delectus beatae, natus exercitationem aspernatur molestias
-            neque reiciendis sed magnam ipsam iusto iure voluptatibus
-            consectetur sequi.
-          </p>
+          <h2>Questions and Answers</h2>
+          <ul>
+            {data.map(item => (
+              <li key={item.id}>
+                <p><strong>Question:</strong> {item.question}</p>
+                <p><strong>Answer:</strong> {item.answer}</p>
+              </li>
+            ))}
+          </ul>
         </div>
       </Container>
     </>
